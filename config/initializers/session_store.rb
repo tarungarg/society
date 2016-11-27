eef0446d70e2644cb39445c3b43feaf9423dccba
Tarun <t.garg28@gmail.com>
2016-11-27 16:26:29 +0530

    key add

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_society_session', domain: '.lvh.me'
+# Rails.application.config.session_store :cookie_store, key: '_society_session', domain: '.lvh.me'
+
+
+Rails.application.config.session_store :cookie_store, key: '_society_session',
+                                                      domain: {
+                                                        production: '.herokuapp.com',
+                                                        development: '.lvh.me'
+                                                      }.fetch(Rails.env.to_sym, :all)
