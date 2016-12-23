12d6ed4901be648c549e3f8e9d55b2ee4c243d83
Tarun <t.garg28@gmail.com>
2016-12-23 17:30:03 +0530

    pagination fix

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,18 +1,18 @@
 document.addEventListener("turbolinks:load", function() {
   if ((location.pathname.indexOf('user_profile') > 0) && $('#infinite-scrolling') && $('#infinite-scrolling').size() > 0) {
-    paginateContent();
+    paginateContent('#infinite-scrolling');
   }
 
   if ((location.pathname.indexOf('timeline') > 0) && $('#timelines-paginate') && $('#timelines-paginate').size() > 0) {
-    paginateContent();
+    paginateContent('#timelines-paginate');
   }
 
 });
 
-function paginateContent(){
+function paginateContent(id){
   $(window).on('scroll', function() {
     var more_posts_url;
-    more_posts_url = $('.pagination a[rel=next]').attr('href');
+    more_posts_url = $(id +' '+'.pagination a[rel=next]').attr('href');
     if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 200) {
       $('.pagination').html('Loading...');
       $.getScript(more_posts_url)
@@ -22,3 +22,7 @@ function paginateContent(){
     }
   });
 }
+
+document.addEventListener("turbolinks:before-cache", function() {
+  $(window).off('scroll')
+});
--- app/controllers/mailbox_controller.rb
+++ app/controllers/mailbox_controller.rb
@@ -1,16 +1,16 @@
 class MailboxController < BaseController
   def inbox
-    @inbox = mailbox.inbox
+    @inbox = mailbox.inbox.paginate(page: params[:page], :per_page => 20)
     @active = :inbox
   end
 
   def sent
-    @sent = mailbox.sentbox
+    @sent = mailbox.sentbox.paginate(page: params[:page], :per_page => 20)
     @active = :sent
   end
 
   def trash
-    @trash = mailbox.trash
+    @trash = mailbox.trash.paginate(page: params[:page], :per_page => 20)
     @active = :trash
   end
 end
