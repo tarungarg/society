c6580ad11b2a7e7a96ea5920e1d4f4e5b062cc07
Tarun <t.garg28@gmail.com>
2016-12-27 18:13:18 +0530

    ass

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,9 +1,11 @@
 module ConversationsHelper
   def recipients_options
     s = ''
-    current_tenant.users.each do |user|
-      s << "<option value='#{user.id}' data-img-src='#{gravatar_image_url(user.email, size: 50)}'>#{user.name} < #{user.email} > </option>"
+    if current_tenant
+      current_tenant.users.each do |user|
+        s << "<option value='#{user.id}' data-img-src='#{gravatar_image_url(user.email, size: 50)}'>#{user.name} < #{user.email} > </option>"
+      end
+      s.html_safe
     end
-    s.html_safe
   end
 end
