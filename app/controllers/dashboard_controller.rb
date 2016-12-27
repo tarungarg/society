27578bddae2a11f2b70a52d90e91d358efff038a
Tarun <t.garg28@gmail.com>
2016-12-27 14:13:07 +0530

    a

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,12 @@ class DashboardController < BaseController
   before_action :authenticate_user!
 
   def index
-    @members_count = current_tenant.users.count
+    @members_count = current_tenant ? current_tenant.users.count : 0
     @mails_count = mailbox.inbox.count
     @complaints_count = current_user.complaints.where(status: 1).count
     @policies_count = Policy.count
     @tasks = current_user.tasks.order('created_at desc')
-    @members = current_tenant.users.order('created_at desc').limit(6)
+    @members = current_tenant ? current_tenant.users.order('created_at desc').limit(6) : []
     @products = Product.order('created_at desc').limit(6)
     @jobs = Job.order('created_at desc').limit(6)
     @suggestions = Suggestion.order('created_at desc').limit(6)
