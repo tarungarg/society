917b330c4ab4d256b5ea0bdb9bfa62ddd25f8173
Tarun <t.garg28@gmail.com>
2016-12-27 18:17:00 +0530

    asd

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -5,7 +5,7 @@ module ConversationsHelper
       current_tenant.users.each do |user|
         s << "<option value='#{user.id}' data-img-src='#{gravatar_image_url(user.email, size: 50)}'>#{user.name} < #{user.email} > </option>"
       end
-      s.html_safe
     end
+    s.html_safe
   end
 end
