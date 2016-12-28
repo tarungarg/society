9fe42a1f320c652fe03522b83cd1104befd8f101
Tarun <t.garg28@gmail.com>
2016-12-28 14:43:30 +0530

    email false

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -1,6 +1,6 @@
 Mailboxer.setup do |config|
   # Configures if your application uses or not email sending for Notifications and Messages
-  config.uses_emails = true
+  config.uses_emails = false
 
   # Configures the default from for emails sent for Messages and Notifications
   config.default_from = 'no-reply@mailboxer.com'
